cbfee76750354951fe2051e31508e1ac6b1a8efc
qrapidnow <qrapidnow@gmail.com>
2024-07-09 08:16:06 +0530

    all

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const itemRouter = require('./routers/item.router');
 require('dotenv').config();
 
 const app = express();
+const allowedOrigins = ['https://frontend1-sand.vercel.app'];
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
@@ -20,8 +21,15 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
   });
 
   app.use(cors({
-    origin: 'https://frontend1-sand.vercel.app/'  // Or use '*' to allow all domains
-}));
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+  }));
 app.use(bodyParser.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(userRouter);

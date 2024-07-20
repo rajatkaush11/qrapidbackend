3514bfd93d866e27fa294cc955d030c8e7e9d195
qrapidnow <qrapidnow@gmail.com>
2024-07-20 16:00:40 +0530

    newbackend

--- app.js
+++ app.js
@@ -14,7 +14,10 @@ const app = express();
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log('MongoDB Connection error', err));
+  .catch(err => {
+    console.error('MongoDB Connection error', err);
+    process.exit(1); // Exit process with failure
+  });
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -27,8 +30,8 @@ app.use(userRouter);
 app.use(authenticate);
 
 app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).json({ error: err.message });
+  console.error(err.stack);
+  res.status(500).json({ error: 'Internal Server Error', details: err.message });
 });
 
 module.exports = app;
--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,5 +1,4 @@
 const UserServices = require('../services/user.services');
-const UserModel = require('../model/user.model');
 const { Clerk } = require('@clerk/clerk-sdk-node');
 
 const clerk = new Clerk({ apiKey: process.env.CLERK_SECRET_KEY });
@@ -21,7 +20,6 @@ exports.register = async (req, res, next) => {
 exports.login = async (req, res, next) => {
   try {
     const { email, password } = req.body;
-
     if (!email || !password) throw new Error('Parameters are not correct');
 
     let user = await UserServices.checkUser(email);
@@ -31,7 +29,6 @@ exports.login = async (req, res, next) => {
     if (!isPasswordCorrect) throw new Error('Username or Password does not match');
 
     const session = await clerk.sessions.createSession({ emailAddress: email, password: password });
-
     let tokenData = { _id: user._id, email: user.email, sessionId: session.id };
     const token = await UserServices.generateAccessToken(tokenData, process.env.JWT_SECRET, '2w');
 

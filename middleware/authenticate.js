8cc9d9c27d8d5157077d310ceb693e8c5c8650f8
qrapidnow <qrapidnow@gmail.com>
2024-07-20 22:17:56 +0530

    now after user

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,19 +1,18 @@
-const jwt = require('jsonwebtoken');
 const UserModel = require('../model/user.model');
 
 const authenticate = async (req, res, next) => {
   try {
-    const token = req.header('Authorization').replace('Bearer ', '');
-    const data = jwt.verify(token, process.env.JWT_SECRET);
+    const clerkId = req.header('Authorization').replace('Bearer ', '');
+    console.log('Received clerkId:', clerkId); // Log clerkId
 
-    const user = await UserModel.findOne({ _id: data._id });
+    const user = await UserModel.findOne({ clerkId });
+    console.log('Authenticated User:', user); // Log user
 
     if (!user) {
       throw new Error();
     }
 
     req.user = user;
-    req.user.clerkId = user.clerkId; // Ensure clerkId is included
     next();
   } catch (error) {
     res.status(401).send({ error: 'Not authenticated' });

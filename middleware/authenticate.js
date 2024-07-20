2085680e4af1d2cb9c3d6255a66a47216a4c82f2
qrapidnow <qrapidnow@gmail.com>
2024-07-20 18:13:21 +0530

    clerk module

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const authenticate = (req, res, next) => {
   const authHeader = req.header('Authorization');
+  console.log('Authorization Header:', authHeader); // Log the Authorization header
   if (!authHeader) return res.status(401).json({ error: 'Access denied. No token provided.' });
 
   const token = authHeader.replace('Bearer ', '');

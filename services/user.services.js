353d4b1309ee2a014257482a714ce31955a074a4
qrapidnow <qrapidnow@gmail.com>
2024-07-21 01:39:17 +0530

    new restaurant data

--- services/user.services.js
+++ services/user.services.js
@@ -25,7 +25,7 @@ class UserServices {
       user = new UserModel({ email, isGoogleUser: true }); // Indicate Google user
       await user.save();
     }
-    const tokenData = { _id: user._id, email: user.email };
+    const tokenData = { _id: user._id, email: user.email, clientId: user.clerkId }; // Include clientId in token
     const token = await this.generateAccessToken(tokenData, process.env.JWT_SECRET, '2w');
     user.token = token;
     await user.save();

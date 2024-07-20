adfc3054c90c6052ed13c78e22f37ba4e8da90da
qrapidnow <qrapidnow@gmail.com>
2024-07-20 19:35:47 +0530

    adding cors

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,11 +1,16 @@
 const UserServices = require('../services/user.services');
-const UserModel = require('../model/user.model');
 
 exports.register = async (req, res, next) => {
   try {
     const { email, password } = req.body;
+    if (!email || !password) {
+      throw new Error('Email and password are required');
+    }
+
     const duplicate = await UserServices.getUserByEmail(email);
-    if (duplicate) throw new Error(`User ${email} already registered`);
+    if (duplicate) {
+      throw new Error(`User ${email} already registered`);
+    }
 
     const user = await UserServices.registerUser(email, password);
     const tokenData = { _id: user._id, email: user.email };
@@ -27,13 +32,19 @@ exports.register = async (req, res, next) => {
 exports.login = async (req, res, next) => {
   try {
     const { email, password } = req.body;
-    if (!email || !password) throw new Error('Parameters are not correct');
+    if (!email || !password) {
+      throw new Error('Parameters are not correct');
+    }
 
     const user = await UserServices.checkUser(email);
-    if (!user) throw new Error('User does not exist');
+    if (!user) {
+      throw new Error('User does not exist');
+    }
 
     const isPasswordCorrect = await user.comparePassword(password);
-    if (!isPasswordCorrect) throw new Error('Username or Password does not match');
+    if (!isPasswordCorrect) {
+      throw new Error('Username or Password does not match');
+    }
 
     const tokenData = { _id: user._id, email: user.email };
     const token = await UserServices.generateAccessToken(tokenData, process.env.JWT_SECRET, '2w');
@@ -42,6 +53,25 @@ exports.login = async (req, res, next) => {
     await user.save();
 
     console.log('User logged in:', user); // Log user details
+    console.log('Token:', token); // Log token
+
+    res.status(200).json({ status: true, success: 'Login successful', token });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+};
+
+exports.googleLogin = async (req, res, next) => {
+  try {
+    const { email } = req.body;
+    if (!email) {
+      throw new Error('Email is required');
+    }
+
+    const { user, token } = await UserServices.registerOrLoginGoogleUser(email);
+
+    console.log('User logged in with Google:', user); // Log user details
     console.log('Token:', token); // Log token
 
     res.status(200).json({ status: true, success: 'Login successful', token });
@@ -67,26 +97,6 @@ exports.getUsers = async (req, res, next) => {
   try {
     const users = await UserModel.find({}, '_id email');
     res.status(200).json(users);
-  } catch (error) {
-    console.error(error);
-    next(error);
-  }
-};
-const UserServices = require('../services/user.services');
-
-exports.googleLogin = async (req, res, next) => {
-  try {
-    const { email } = req.body;
-    if (!email) {
-      throw new Error('Email is required');
-    }
-
-    const { user, token } = await UserServices.registerOrLoginGoogleUser(email);
-
-    console.log('User logged in with Google:', user); // Log user details
-    console.log('Token:', token); // Log token
-
-    res.status(200).json({ status: true, success: 'Login successful', token });
   } catch (error) {
     console.error(error);
     next(error);

c26d4a2ca4fdd21e84f52effa4903f1a79c47361
qrapidnow <qrapidnow@gmail.com>
2024-07-21 17:52:49 +0530

    new

--- routers/user.router.js
+++ routers/user.router.js
@@ -5,9 +5,6 @@ const UserController = require('../controller/user.controller');
 // Google login route
 router.post('/googleLogin', UserController.googleLogin);
 
-// Create or update user with Clerk ID
-router.post('/createOrUpdateUser', UserController.createOrUpdateUser);
-
 // Other user-related routes can be added here
 
 module.exports = router;

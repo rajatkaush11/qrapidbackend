89301367a8707400c8490a104d087fc80005f803
qrapidnow <qrapidnow@gmail.com>
2024-07-21 17:32:52 +0530

    new

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
-const { users } = require('@clerk/clerk-sdk-node');
 
 dotenv.config();
 

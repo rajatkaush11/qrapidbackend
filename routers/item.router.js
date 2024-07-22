fa41370d2b6e92edc7efa41d40e1a161e224be88
qrapidnow <qrapidnow@gmail.com>
2024-07-22 19:15:04 +0530

    updated delete item button

--- routers/item.router.js
+++ routers/item.router.js
@@ -6,5 +6,6 @@ const ItemController = require('../controller/item.controller');
 
 router.post('/items', authenticate, upload.single('image'), ItemController.createItem);
 router.get('/items/:categoryId', authenticate, ItemController.getItemsByCategory);
+router.delete('/items/:id', authenticate, ItemController.deleteItem); // Add this route
 
 module.exports = router;

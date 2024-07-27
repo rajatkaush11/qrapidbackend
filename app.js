99a3ec485fbb68f89d3d9c851a1648b7d1522dc7
qrapidnow <qrapidnow@gmail.com>
2024-07-27 17:22:33 +0530

    fix spacing

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const itemRouter = require('./routers/item.router');
 require('dotenv').config();
 
 const app = express();
-const allowedOrigins = ['https://qrapidwebsite.vercel.app', 'https://digitalmenu-rouge.vercel.app'];
+const allowedOrigins = ['https://qr-dashboard-1107.web.app', 'https://digitalmenu-rouge.vercel.app'];
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {

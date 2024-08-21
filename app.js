3a6b1df6d6724726eb369cbe3f1121c98ddc1568
qrapidnow <qrapidnow@gmail.com>
2024-08-22 02:13:52 +0530

    new token

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const userRouter = require('./routers/user.router');
 const categoryRouter = require('./routers/category.router');
 const itemRouter = require('./routers/item.router');
 const restaurantRouter = require('./routers/restaurant.router');
+const orderRouter = require('./routers/order.router'); // Import the order router
 require('dotenv').config();
 
 const app = express();
@@ -37,6 +38,7 @@ app.use(userRouter);
 app.use(categoryRouter);
 app.use(itemRouter);
 app.use(restaurantRouter);
+app.use(orderRouter); // Use the order router
 
 app.use((err, req, res, next) => {
     console.error('Global error handler:', err);
